1ef6d9972687d50aa74ccdfff6b7117f69f858d4
michaelcini <michaelcini1@gmail.com>
2025-09-07 11:48:21 +0200

    Update stations_repository.dart

--- lib/stations_repository.dart
+++ lib/stations_repository.dart
@@ -3,25 +3,41 @@ import 'package:http/http.dart' as http;
 import 'models.dart';
 
 class StationsRepository {
-  // Load stations from a remote M3U/PLS file
+  final List<Station> _stations = [];
+
+  List<Station> get stations => List.unmodifiable(_stations);
+
   Future<List<Station>> loadStations(String url) async {
     final uri = Uri.parse(url);
     final resp = await http.get(uri);
 
     if (resp.statusCode == 200) {
       final body = utf8.decode(resp.bodyBytes);
-      // TODO: parse M3U into stations
-      // For now return a placeholder
-      return [
-        Station(name: "Sample Station", url: url),
-      ];
+      // TODO: parse M3U/PLS/OPML here
+      final station = Station(
+        id: uri.toString(),
+        name: "Sample Station",
+        url: uri.toString(),
+      );
+      _stations.add(station);
+      return _stations;
     } else {
       throw Exception("Failed to load stations: ${resp.statusCode}");
     }
   }
 
-  // Save stations locally (to be implemented with SharedPreferences later)
   Future<void> saveStations(List<Station> stations) async {
-    // Placeholder: no-op for now
+    // TODO: persist with SharedPreferences
+  }
+
+  void addLink(String name, String url, {String? logo}) {
+    _stations.add(
+      Station(
+        id: url,
+        name: name,
+        url: url,
+        logo: logo,
+      ),
+    );
   }
 }
