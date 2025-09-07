c74c4a44b8d12adbb075d79b9f9ada3145980444
michaelcini <michaelcini1@gmail.com>
2025-09-07 12:29:59 +0200

    Update audio_handler.dart

--- lib/audio_handler.dart
+++ lib/audio_handler.dart
@@ -2,7 +2,7 @@ import 'package:audio_service/audio_service.dart';
 import 'package:just_audio/just_audio.dart';
 import 'models.dart';
 
-/// Main Audio Handler for RadioAutoM3u
+/// Main Audio Handler for RadioAutoM3U
 class RadioAudioHandler extends BaseAudioHandler with QueueHandler, SeekHandler {
   final _player = AudioPlayer();
 
@@ -39,5 +39,70 @@ class RadioAudioHandler extends BaseAudioHandler with QueueHandler, SeekHandler 
 
   /// Play a specific [Station] from queue
   Future<void> playStation(Station station) async {
-    final mediaItem = MediaItem(
+    final item = MediaItem(
       id: station.url,
+      album: "RadioAutoM3U",
+      title: station.name,
+      artUri: station.logo != null ? Uri.parse(station.logo!) : null,
+    );
+
+    // Update current media item (for Android Auto & notification)
+    this.mediaItem.add(item);
+
+    try {
+      await _player.setUrl(station.url);
+      await _player.play();
+    } catch (e) {
+      print("Error playing station: $e");
+    }
+  }
+
+  @override
+  Future<void> play() => _player.play();
+
+  @override
+  Future<void> pause() => _player.pause();
+
+  @override
+  Future<void> stop() => _player.stop();
+
+  @override
+  Future<void> seek(Duration position) => _player.seek(position);
+
+  @override
+  Future<void> skipToNext() async {
+    if (queue.value.isNotEmpty) {
+      final currentIndex = queue.value.indexOf(mediaItem.value!);
+      final nextIndex = (currentIndex + 1) % queue.value.length;
+      final next = queue.value[nextIndex];
+      mediaItem.add(next);
+      await _player.setUrl(next.id);
+      play();
+    }
+  }
+
+  @override
+  Future<void> skipToPrevious() async {
+    if (queue.value.isNotEmpty) {
+      final currentIndex = queue.value.indexOf(mediaItem.value!);
+      final prevIndex =
+          (currentIndex - 1 + queue.value.length) % queue.value.length;
+      final prev = queue.value[prevIndex];
+      mediaItem.add(prev);
+      await _player.setUrl(prev.id);
+      play();
+    }
+  }
+
+  /// Play from Android Auto (by Media ID = URL)
+  @override
+  Future<void> playFromMediaId(String mediaId,
+      [Map<String, dynamic>? extras]) async {
+    try {
+      await _player.setUrl(mediaId);
+      await _player.play();
+    } catch (e) {
+      print("Error in playFromMediaId: $e");
+    }
+  }
+}
